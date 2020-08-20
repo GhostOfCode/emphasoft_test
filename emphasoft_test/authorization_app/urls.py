2bc75fa5a6a2df3d81b2be0647e759ac6d61764c
Nikita <nikitos_athletos@mail.ru>
2020-08-21 00:25:28 +0500

    added friend_list

--- emphasoft_test/authorization_app/urls.py
+++ emphasoft_test/authorization_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import Authorization, Profile
 
 urlpatterns = [
-    path('', Authorization.as_view()),
+    path('auth/', Authorization.as_view()),
     path('', include('social_django.urls')),
     path('accounts/profile/', Profile.as_view())
 ]
--- emphasoft_test/authorization_app/views.py
+++ emphasoft_test/authorization_app/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
+from django.template import Context
 from django.views import View
+
+import vk_api
 
 
 class Authorization(View):
-
     def get(self, request):
         return render(request, 'authorization_app/authorization.html')
 
 
 class Profile(View):
     def get(self, request):
-        print(request)
-        return render(request, 'profile.html')
+        friends = self.get_friends_list(request.user)
+        q = Context(friends)
+        return render(request, 'profile.html', context={'friends_list': friends})
+
+    def get_friends_list(self, user):
+        social = user.social_auth.get()
+        access_token = social.extra_data['access_token']
+        api = vk_api.VkApi(token=access_token, login=user.username).get_api()
+        friends_list = api.friends.get(order='random', count=5, fields=['photo_100'])
+        return friends_list['items']
+
